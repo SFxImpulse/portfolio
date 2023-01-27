d24f88fbae8e3746432748fb1906e395a53c2e00
SFxImpulse <braverman.david.business@gmail.com>
2023-01-27 16:48:37 -0500

    add buttons for dark-mode and menu dropdown.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import Button from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGripLines } from "@fortawesome/free-solid-svg-icons";
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import "./Header.scss";
 
 const Header = () => {
@@ -6,8 +9,8 @@ const Header = () => {
     <nav className="nav-container">
       <h2 className="last-name">Braverman</h2>
       <div className="button-container">
-        <button>Light/Dark mode</button>
-        <button>Drop Down Menu</button>
+        <FontAwesomeIcon icon={faMoon} size="2x" className="darkmode-button" onClick={() => console.log("Dark Mode Activated")} />
+        <FontAwesomeIcon icon={faGripLines} size="2x" className="menu-button" onClick={() => console.log("Menu Opened")} />
       </div>
     </nav>
   );
