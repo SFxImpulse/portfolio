73cc24a9a4bbd61a136ac00fea42e0f5cd371b51
SFxImpulse <braverman.david.business@gmail.com>
2023-01-29 13:07:49 -0500

    change font-size.

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -14,8 +14,8 @@ function MainPage(props) {
         <h2 className="title text--regular">Full Stack Web Developer</h2>
         <h1 className="name text--bold">David Braverman</h1>
         <h4 className="bio text--light">I am a full stack web developer based in Toronto Ontario. I love video games, making music, and my lovely animals.</h4>
-        {/* <img src="">This is an image of me being professional</img> */}
       </section>
+      {/* <img src="itsa me"></img> */}
     </div>
   )
 };
